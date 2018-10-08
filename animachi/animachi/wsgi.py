686b3253335f9d78b4b5c15d0bf804a1ac723e6c
kanade <melty0404@gmail.com>
2018-10-08 21:25:27 +0900

    [ADD] 定期的にアクセスするスクリプト（herokuの無料枠だと落ちる）

--- animachi/animachi/wsgi.py
+++ animachi/animachi/wsgi.py
@@ -8,9 +8,27 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
+import threading
+import requests
+import time
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'animachi.settings')
 
 application = Cling(get_wsgi_application())
+
+
+def awake():
+    while True:
+        try:
+            print('Start Awaking')
+            requests.get('http://animachi.herokuapp.com/')
+            print('End')
+        except:
+            print('error')
+        time.sleep(300)
+
+
+t = threading.Thread(target=awake)
+t.start()
